cec97b5268c3becb6cb4bc54430f2e9ab2e4be1b
Min Soung Choi <m.choi960@gmail.com>
2020-07-16 22:37:46 -0600

    added chip8 fontset

--- src/chip8.h
+++ src/chip8.h
@@ -5,13 +5,13 @@ class Chip8 {
   unsigned char memory[4096];
   unsigned char V[16];
 
-  unsigned short I;
+  unsigned short index;
   unsigned short pc;
 
   unsigned char gfx[64 * 32];
 
-  unsigned char delay_timer;
-  unsigned char sound_timer;
+  unsigned char delayTimer;
+  unsigned char soundTimer;
 
   unsigned short stack[16];
   unsigned short sp;
@@ -19,7 +19,7 @@ class Chip8 {
   unsigned char key[16];
 
   public:
-    bool draw_flag;
-    void init();
-    void emulate_cycle();
+    bool drawFlag;
+    void initialize();
+    void emulateCycle();
 };
